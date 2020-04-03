5112a77a2b6cd7139ae99afd5ced5f3f22492e10
Haaris <thecanoechief@gmail.com>
2020-04-03 11:08:21 -0500

    git commit 	modified:   2-JS-basics/starter/index.html 	new file:   2-JS-basics/starter/script.js

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,14 @@
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = "20";
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
